c550770463a503baaa166cd9147c8e98c7085f79
Eugene <hmkwon@pxscope.com>
2024-09-25 16:54:50 +0900

    change test code

--- tests/test.py
+++ tests/test.py
@@ -1,16 +1,22 @@
 from video_client import *
 
+def on_data(ctx, data, size, frame_info):
+    print(f"Received data: size={size}, frame_info={frame_info}")
+
+def on_disconnect(ctx, code, msg):
+    print(f"Disconnected: code={code}, msg={msg}")
+
 api_init()
 clnt = create_video_client()
 if clnt:
-    ret = connect_video_client(clnt, "", 3)
+    ret = connect_video_client(clnt, "", 3, on_disconnect)
     if ret == ApiError.SUCCESS.value:
         dec_ctx = VideoProcContext()
         dec_ctx.gpu_index = 0
         dec_ctx.target_format = PixelFormat.RGB24
         dec_ctx.target_fps = 30
 
-        start_video_client(clnt, dec_ctx, on_data_callback)
+        start_video_client(clnt, dec_ctx, on_disconnect)
     else:
         print(ret)
 
